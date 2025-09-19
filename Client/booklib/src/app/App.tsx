9711b50cd3c2701ee02c9013f8d8d6a89272f272
fefelad <serzh.mr.s.luzin@mail.ru>
2025-09-19 16:23:48 +0300

    project init

---
+++ Client/booklib/src/app/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <>
+      <div className={styles.wrapper_app}>
+        <p>12321</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
