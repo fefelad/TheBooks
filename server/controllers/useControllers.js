eda7ce6d99acd6203e4079f10698006d5d41df1b
fefelad <serzh.mr.s.luzin@mail.ru>
2025-09-23 17:44:47 +0300

    create registerUser Controllers

--- server/controllers/useControllers.js
+++ server/controllers/useControllers.js
@@ -1,7 +1,50 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const bcrypt = require("bcrypt");
+const Jdenticon = require("jdenticon");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const path = require("path");
 class UserController {
   async RegisterUser(req, res) {
-    res.send("Register User");
+    const { name, password, email } = req.body;
+
+    if (!name || !password) {
+      return res.status(400).json({ message: "Все поля обязательны" });
+    }
+
+    try {
+      const existingUser = await prisma.user.findUnique({
+        where: { email: email },
+      });
+
+      if (existingUser) {
+        return res.status(400).json({ message: "Пользователь уже существует" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const png = Jdenticon.toPng(name, 200);
+      const avatarName = `${name}_${Date.now()}.png`;
+      const avatarPath = path.join(__dirname, "../uploads", avatarName);
+      fs.writeFileSync(avatarPath, png);
+
+      const user = await prisma.user.create({
+        data: {
+          username: name,
+          passwordHash: hashedPassword,
+          avatarUrl: `/static/${avatarPath}`, // поправил здесь, чтобы путь был относительный и правильный
+          email: email,
+        },
+      });
+
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Ошибка сервера" });
+    }
   }
+
   async LoginUser(req, res) {
     res.send("Login User");
   }
@@ -11,4 +54,9 @@ class UserController {
   async getUserId(req, res) {
     res.send("Get User by ID");
   }
+  async updateUser(req, res) {
+    res.send("Update User");
+  }
 }
+
+module.exports = UserController;
